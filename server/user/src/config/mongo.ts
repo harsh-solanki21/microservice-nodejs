76ba053905fad415331f69817bd13a5b9c8cfb3c
Harsh Solanki <harsh.solanki2105@gmail.com>
2023-12-17 14:46:00 +0530

    user schema added

--- server/user/src/config/mongo.ts
+++ server/user/src/config/mongo.ts
@@ -1,9 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose, { ConnectOptions } from 'mongoose'
+import dotenv from 'dotenv'
 
 const connectMongo = async () => {
-    try {
-
-    } catch (error) {
-        throw new 
-    }
+  try {
+    await mongoose.connect(
+      process.env.MONGO_URI as string,
+      { useNewUrlParser: true, useUnifiedTopology: true } as ConnectOptions
+    )
+  } catch (error) {
+    console.log(error)
+    process.exit(1)
+  }
 }
--- server/user/src/model/User.ts
+++ server/user/src/model/User.ts
@@ -1,1 +1,39 @@
 import { Schema } from 'mongoose'
+import { IUser } from '../interface/user'
+
+const UserSchema: Schema = new Schema(
+  {
+    user_no: {
+      type: Number,
+    },
+    name: {
+      type: String,
+      required: [true, 'Name is Required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      lowercase: true,
+      trim: true,
+      unique: true,
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        'Please fill Valid Email',
+      ],
+      required: [true, 'Email is Required'],
+    },
+    password: {
+      type: String,
+      trim: true,
+      minLength: [4, 'Password must be atleast 6 characters long'],
+      required: [true, 'Password is Required'],
+    },
+  },
+  {
+    timestamp: true,
+  }
+)
+
+UserSchema.index({ email: 1 }, { unique: true })
+
+export default model<IUser>('users', UserSchema)
